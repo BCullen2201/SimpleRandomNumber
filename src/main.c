4402a8c7d556b4365be9d99f21596f1695b03dc6
Brandon Cullen <brandoncullen2201@gmail.com>
2025-08-17 14:20:47 -0400

    User can now choose min and max numbers

--- src/main.c
+++ src/main.c
@@ -3,8 +3,17 @@
 
 #include "randNumGen.h"
 
-int main() {
-	long long int x = getRandomNumber(-1000000,1000000);
+int main(int argc, char *argv[]) {
+	if (argc != 3 || atoi(argv[1]) == 0 || atoi(argv[2]) == 0) {
+		printf("Usage: %s [integer] [integer]\n\nFirst integer should be less than the second.\n", argv[0]);
+		exit(1);
+	}
+
+	long long int int1, int2;
+	int1 = atoi(argv[1]);
+	int2 = atoi(argv[2]);
+
+	long long int x = getRandomNumber(int1, int2);
 	printf("%lld\n", x);
 
     return 0;
